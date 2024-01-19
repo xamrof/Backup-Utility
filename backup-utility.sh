70a52bcf80b40bd7694ff6eba830e47603dc791f
Maxwell <codingxam@gmail.com>
2024-01-18 21:57:14 -0400

    resolviendo ejercicio

--- backup-utility.sh
+++ backup-utility.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # Directorio de origen que deseas respaldar
-origen="/ruta/al/directorio/a/respaldar"
+origen="/home/kali/Desktop/OriginData"
 
 # Directorio de destino donde se almacenarán las copias de seguridad
-destino="/ruta/al/directorio/de/respaldo"
-
+destino="/home/kali/backups"
 # Nombre del archivo de copia de seguridad con marca de tiempo
-archivo_destino="$destino/respaldo_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
+nombre_archivo="respaldo_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz"
 
 # Comprimir y copiar los archivos de origen al directorio de destino
-tar -czvf "$archivo_destino" "$origen"
+tar -czvf "$destino/$nombre_archivo" -C "$origen" .
+
+cd $destino
+
+curl -T $nombre_archivo ftp://ip-server --user user:pass
 
 # Comprobar si la copia de seguridad se realizó con éxito
 if [ $? -eq 0 ]; then
-  echo "Copia de seguridad exitosa en $archivo_destino"
+  echo "Copia de seguridad exitosa en $destino"
 else
   echo "Error al realizar la copia de seguridad."
 fi
